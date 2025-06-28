e178fc448bcc4339e93b6995bdeb2469923728be
krishthakur69 <krishthakur4924@gmail.com>
2025-06-27 23:21:52 -0700

    Arithematic , Assignment , Comparison , Logical operations

---
+++ 04_operators.py
@@ -0,0 +1,35 @@
+# Arithematic operators
+
+a = 34
+ 
+b = 2 
+
+c = a+b
+
+print(c)
+
+
+# Assignment operators 
+a = 4-2 # Assign 4-2 in a 
+print(a)
+b = 6 
+b+= 3 # Increment the value of b by 3 then assign it to b 
+print(b)
+
+
+
+
+# comparison operators 
+
+a = 5>4
+print(a)
+
+a = 5==7
+print(a) 
+
+
+#logical operators 
+
+e = True or False
+print(e)
+
